5da3a913512f45b9ced7f84f772ef2a2e0cc77c4
Dangerous5677 <109791541+Dangerous5677@users.noreply.github.com>
2024-08-23 22:34:03 +0500

    ♥️˻𝗦𝝜𝗔𝗡𝗜-𝗠𝗗-:𝟗𝟗𝟒˼♥️

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -3,9 +3,9 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
     // Sound
     let name = m.pushName || conn.getName(m.sender)
     var vn = "https://cdn.jsdelivr.net/gh/Itxmanno05/GURU-BOT@main/Assets/mp3/sajjad.m4a"
-    let url = "https://github.com/MANNO-GDS/THE-MANNO-MD"
+    let url = "https://github.com/SHANI-GDS/SHANI-MD"
     let murl = "https://youtu.be/DibiLc17dh0?si=xp9bQ-_frEyDB1-i"
-    let img = "https://telegra.ph/file/d52a552a0b817753754e8.jpg"
+    let img = "https://i.imgur.com/Mbnk3am.jpeg"
     let con = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
     let doc = {
         audio: {
@@ -19,8 +19,8 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
         contextInfo: {
           mentionedJid: [m.sender],
           externalAdReply: {
-          title: "THE-MANNO-BOT IS ALIVE 💌💭",
-          body: "MANNO BOT",
+          title: "SHANI-MD-BOT IS ALIVE 💌💭",
+          body: "SHANI-MD",
           thumbnailUrl: img,
           sourceUrl: 'https://chat.whatsapp.com/BNE0V8XpEZK0q4IgJ9jklM',
           mediaType: 1,
