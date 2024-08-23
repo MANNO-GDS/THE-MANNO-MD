89bb561d329e0b6d6fba2bf41a116ffee2f8fa44
SHANI-RAJPOOT <109791541+SHANI-GDS@users.noreply.github.com>
2024-08-24 01:01:50 +0500

    ♥️˻𝗦𝝜𝗔𝗡𝗜-𝗠𝗗-:𝟗𝟗𝟒˼♥️

--- config.js
+++ config.js
@@ -91,16 +91,16 @@ global.APIKeys = {
 
 // Bot Images 
 global.imagen1 = fs.readFileSync("./Assets/menus/Menu.png")
-global.imagen2 = fs.readFileSync("./Assets/menus/Menu1.jpg")
-global.imagen3 = fs.readFileSync("./Assets/menus/Menu2.jpg")
-global.imagen4 = fs.readFileSync("./Assets/menus/Menu3.jpg")
-global.imagen5 = fs.readFileSync("./Assets/menus/img2.jpg")
-global.imagen6 = fs.readFileSync("./Assets/menus/img5.jpg")
-global.imagen7 = fs.readFileSync("./Assets/menus/img6.jpg")
-global.imagen8 = fs.readFileSync("./Assets/menus/img8.jpg")
-global.imagen9 = fs.readFileSync("./Assets/menus/img9.jpg")
-global.imagen10 = fs.readFileSync("./Assets/menus/img11.jpg")
-global.imagen11 = fs.readFileSync("./Assets/menus/img12.jpg")
+global.imagen2 = fs.readFileSync("./Assets/menus/Menu.png")
+global.imagen3 = fs.readFileSync("./Assets/menus/Menu.png")
+global.imagen4 = fs.readFileSync("./Assets/menus/Menu.png")
+global.imagen5 = fs.readFileSync("./Assets/menus/Menu.png")
+global.imagen6 = fs.readFileSync("./Assets/menus/Menu.png")
+global.imagen7 = fs.readFileSync("./Assets/menus/Menu.png")
+global.imagen8 = fs.readFileSync("./Assets/menus/Menu.png")
+global.imagen9 = fs.readFileSync("./Assets/menus/Menu.png")
+global.imagen10 = fs.readFileSync("./Assets/menus/Menu.png")
+global.imagen11 = fs.readFileSync("./Assets/menus/Menu.png")
 //💌------------------------------------------💌
 
 
@@ -134,7 +134,7 @@ global.menuvid = 'https://i.imgur.com/8E2itvZ.mp4'
 global.Princesc = 'https://github.com/SHANI-GDS/SHANI-MD' 
 global.princeyt = 'https://youtube.com/'
 global.Princelog = 'https://i.imgur.com/Mbnk3am.jpeg'
-global.thumb = fs.readFileSync('./Assets/Prince.png')
+global.thumb = fs.readFileSync('./Assets/Shani.png')
 //💌------------------------------------------💌
 
 
