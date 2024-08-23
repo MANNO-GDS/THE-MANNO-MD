61940ec23b8c3329cc3f045a0d2c982826a9ea02
Dangerous5677 <109791541+Dangerous5677@users.noreply.github.com>
2024-08-23 21:59:46 +0500

    ♥️˻𝗦𝝜𝗔𝗡𝗜-𝗠𝗗-:𝟗𝟗𝟒˼♥️

--- plugins/bebot-stopbot.js
+++ plugins/bebot-stopbot.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn }) => {
    await conn.reply(m.chat, '💭 𝙒𝙃𝙔 𝙉𝙊𝙏 𝙂𝙊 𝘿𝙄𝙍𝙄𝘾𝙏𝙇𝙔 𝙏𝙊 𝙏𝙃𝙀 𝙏𝙀𝙍𝙈𝙄𝙉𝘼𝙇 ?', m);
   } else {
     //Si el número no coincide, se detiene el bot.
-    await conn.reply(m.chat, `✅ 𝘽𝙊𝙏 𝙄𝙎 𝘾𝙊𝙉𝙉𝙀𝘾𝙏𝙀𝘿`, m);
+    await conn.reply(m.chat, `✅ 𝘽𝙊𝙏 𝙄𝙎 𝘿𝙄𝙎𝘾𝙊𝙉𝙉𝙀𝘾𝙏𝙀𝘿`, m);
     conn.ws.close();
   }
 };
