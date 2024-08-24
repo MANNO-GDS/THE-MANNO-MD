f969c51b22d6ca34f7abc4471a21a219e5ee06bb
SHANI-RAJPOOT <109791541+SHANI-GDS@users.noreply.github.com>
2024-08-24 14:39:40 +0500

    Update dl-modapk.js

--- plugins/dl-modapk.js
+++ plugins/dl-modapk.js
@@ -20,7 +20,7 @@ let handler = async (m, { conn, command, usedPrefix, text }) => {
         let sections = [
             {
                 title: '𝗦𝝜𝗔𝗡𝗜-𝗠𝗗',
-                highlight_label: '🦊˻𝗦-𝗠𝗗🦊',
+                highlight_label: '♥ 𝗦-𝗠𝗗 ♥',
                 rows: [{
                     header: '👇🏻𝗙𝗢𝗥 𝗕𝗢𝗧 𝗠𝗘𝗡𝗨 𝗦𝗘𝗟𝗘𝗖𝗧 𝗧𝗛𝗜𝗦',
                     title: "𝗠𝗘𝗡𝗨",
@@ -60,10 +60,10 @@ let handler = async (m, { conn, command, usedPrefix, text }) => {
                             text: "𝗖𝗟𝗜𝗖𝗞 𝗢𝗡 𝗧𝗛𝗘 𝗕𝗨𝗧𝗧𝗢𝗡 𝗕𝗘𝗟𝗢𝗪 𝗧𝗢 𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗 𝗬𝗢𝗨𝗥 𝗔𝗣𝗞 "
                         }),
                         footer: proto.Message.InteractiveMessage.Footer.create({
-                            text: '♥️˻𝗦-𝗠𝗗♥️'
+                            text: '♥️ 𝗦-𝗠𝗗 ♥️'
                         }),
                         header: proto.Message.InteractiveMessage.Header.create({
-                            subtitle: '♥️˻𝗦-𝗠𝗗♥️',
+                            subtitle: '♥️ 𝗦-𝗠𝗗 ♥️',
                             hasMediaAttachment: false
                         }),
                         nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
